4c6fe36f24ffe12868d143907770cdbdafcf9d1e
JiaMing991203 <tomyao991203@gmail.com>
2024-04-20 22:42:39 -0400

    changed rope __init__ (start from numpy then torch)

--- atten_model.py
+++ atten_model.py
@@ -71,17 +71,20 @@ class RotaryPositionalEmbedding(nn.Module):
         super(RotaryPositionalEmbedding, self).__init__()
 
         # Create a rotation matrix.
-        self.device = device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.rotation_matrix = torch.zeros(d_model, d_model, device=torch.device(self.device))
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        rotation_matrix = np.zeros(d_model, d_model)
         for i in range(d_model):
             for j in range(d_model):
-                self.rotation_matrix[i, j] = torch.cos(i * j * 0.01)
+                rotation_matrix[i, j] = np.cos(i * j * 0.01)
+        self.rotation_matrix = torch.from_numpy(rotation_matrix).to(self.device)
 
         # Create a positional embedding matrix.
-        self.positional_embedding = torch.zeros(max_seq_len, d_model, device=torch.device(self.device))
+        positional_embedding = np.zeros(max_seq_len, d_model)
         for i in range(max_seq_len):
             for j in range(d_model):
-                self.positional_embedding[i, j] = torch.cos(i * j * 0.01)
+                positional_embedding[i, j] = np.cos(i * j * 0.01)
+        self.positional_embedding = torch.from_numpy(positional_embedding).to(self.device)
 
     def forward(self, x):
         """
